9eaa5ffd1ca70337c6dcfa508440f6db60c9fc81
Joseph Roque <contact@josephroque.ca>
2016-02-27 22:35:39 -0800

    Created class and test case for creating pizzas and gitpizza status

---
+++ gitpizza.py
@@ -0,0 +1,227 @@
+import sys
+
+# Available sizes
+sizes = {'personal', 'small', 'medium', 'large'}
+# Available pizza bases
+bases = {'pan', 'homestyle', 'edge', 'stuffed', 'thin'}
+# Available sauces
+sauces = {'tomato', 'alfredo', 'beef-gravy', 'indian-butter', 'old-world-tomato'}
+# Available cheeses
+cheeses = {'regular', 'remove', 'light', 'extra'}
+
+# Sides that toppings can be added to
+sides = {'left', 'right', 'both'}
+
+# List of available meat options
+meats = ['pepperoni', 'bacon-crumble', 'bacon', 'ham', 'grilled-chicken-breast', 'italian-sausage', 'mild-sausage', 'beef', 'smokey-maple-bacon']
+# List of available veggies options
+veggies = ['mushroom', 'green-pepper', 'pineapple', 'red-onions', 'roasted-red-peppers', 'hot-peppers', 'tomatoes', 'marinated-tomatoes', 'black-olives', 'roasted-garlic', 'parmesan', 'cheddar-cheese', 'cheese-curds', 'feta-cheese', 'pesto']
+
+# Pizza object
+class Pizza(object):
+
+    # Declares default pizza options
+    def __init__(self):
+        self.meats = {'left': set(), 'right': set()}
+        self.veggies = {'left': set(), 'right': set()}
+        self.cheese = {'left': 'regular', 'right': 'regular'}
+        self.size = 'personal'
+        self.base = 'pan'
+        self.sauce = 'tomato'
+
+    def change_size(self, size):
+        # Check if the size is valid and if so, update the pizza
+        if size not in sizes:
+            return False
+        self.size = size
+        return True
+
+    def change_base(self, base):
+        # Check if the base is valid and if so, update the pizza
+        if base not in bases:
+            return False
+        self.base = base
+        return True
+
+    def change_sauce(self, sauce):
+        # Check if the sauce type is valid and if so, update the pizza
+        if sauce not in sauces:
+            return False
+        self.sauce = sauce
+        return True
+
+    def change_cheese(self, side_arg, cheese):
+        # If the side is not 'left', 'right', or 'both', return False
+        if side_arg not in sides:
+            return False
+
+        # Create a list of the sides the cheese is being changed on
+        if side_arg == 'both':
+            side_arg = sides
+        else:
+            side_arg = [side]
+
+        # Make sure the cheese is a valid type
+        if cheese not in cheeses:
+            return False
+
+        # Update the cheese
+        for side in side_arg:
+            self.cheese[side] = cheese
+        return True
+
+
+    def add_topping(self, side_arg, topping):
+        # If the side is not 'left', 'right', or 'both', return False
+        if side_arg not in sides:
+            return False
+
+        # Create a list of the sides the topping is being added to
+        if side_arg == 'both':
+            side_arg = sides
+        else:
+            side_arg = [side_arg]
+
+        # Find the index of the topping in the meat or veggies list, or return False if it isn't found
+        topping_list = None
+        if topping in meats:
+            topping_list = self.meats
+        elif topping in veggies:
+            topping_list = self.veggies
+        else:
+            return False
+
+        # Adding the topping to the specified sides
+        for side in side_arg:
+            topping_list[side].add(topping)
+
+        return True
+
+    def remove_topping(self, side_arg, topping):
+        # If the side is not 'left' or 'right', return False
+        if side_arg not in sides:
+            return False
+
+        # Create a list of the sides the topping is being added to
+        if side_arg == 'both':
+            side_arg = sides
+        else:
+            side_arg = [side]
+
+        if topping not in meats and topping not in veggies:
+            return False
+
+        # Adding the topping to the specified sides
+        for side in side_arg:
+            self.meats[side].discard(topping)
+            self.veggies[side].discard(topping)
+        return True
+
+    def get_status(self):
+        # Compile a string containing the left meats
+        if len(self.meats['left']) == 0:
+            left_meats = "\tNone"
+        else:
+            first_meat = self.meats['left'].pop()
+            left_meats = "\t{0}".format(first_meat)
+            for meat in self.meats['left']:
+                left_meats += "\n\t\t\t{0}".format(meat)
+            self.meats['left'].add(first_meat)
+
+        # Compile a string containing the left veggies
+        if len(self.veggies['left']) == 0:
+            left_veggies = "\tNone"
+        else:
+            first_veggie = self.veggies['left'].pop()
+            left_veggies = "\t{0}".format(first_veggie)
+            for veggie in self.veggies['left']:
+                left_veggies += "\n\t\t\t{0}".format(veggie)
+            self.veggies['left'].add(first_veggie)
+
+        # Compile a string containing the right meats
+        if len(self.meats['right']) == 0:
+            right_meats = "\tNone"
+        else:
+            first_meat = self.meats['right'].pop()
+            right_meats = "\t{0}".format(first_meat)
+            for meat in self.meats['right']:
+                right_meats += "\n\t\t\t{0}".format(meat)
+            self.meats['right'].add(first_meat)
+
+        # Compile a string containing the right veggies
+        if len(self.veggies['right']) == 0:
+            right_veggies = "\tNone"
+        else:
+            first_veggie = self.veggies['right'].pop()
+            right_veggies = "\t{0}".format(first_veggie)
+            for veggie in self.veggies['right']:
+                right_veggies += "\n\t\t\t{0}".format(veggie)
+            self.veggies['right'].add(first_veggie)
+
+        properties = (
+            "\n\tYour pizza so far:\n"
+            "\t------------------------------\n"
+            "\tSize:\t\t{0}\n"
+            "\tBase:\t\t{1}\n"
+            "\tSauce:\t\t{2}\n"
+            "\t------------------------------\n"
+            "\tLeft cheese:\t{3}\n"
+            "\tLeft Meats:"
+            "{4}\n"
+            "\tLeft Veggies:"
+            "{5}\n"
+            "\t------------------------------\n"
+            "\tRight cheese:\t{6}\n"
+            "\tRight Meats:"
+            "{7}\n"
+            "\tRight Veggies:"
+            "{8}\n"
+        )
+
+        properties = properties.format(
+            self.size,
+            self.base,
+            self.sauce,
+            self.cheese['left'],
+            left_meats,
+            left_veggies,
+            self.cheese['right'],
+            right_meats,
+            right_veggies)
+
+
+        print(properties)
+
+        # '\n\t\t\t'.join(self.meats['left'])
+        # '\t\tLeft Veggies:\n'
+        # '\n\t\t\t'.join(self.veggies['left'])
+        #
+        # '\t\tRight cheese:\n'
+        # '\t\tRight Meats:\n'
+        # '\n\t\t\t'.join(self.meats['right'])
+        # '\t\tRight Veggies:\n'
+        # '\n\t\t\t'.join(self.veggies['right'])
+
+# def create_new_pizza():
+
+pizza = Pizza()
+pizza.add_topping('left', 'pepperoni')
+pizza.add_topping('right', 'ham')
+pizza.add_topping('right', 'pineapple')
+pizza.get_status()
+
+# Argument parsing
+# def parse_first_arg(arg):
+#     if 'init' == arg:
+#         create_new_pizza()
+#
+#
+#
+# # Tool
+# if len(sys.argv) <= 1:
+#     print('\tWelcome to git-pizza.')
+#     print('\tTo get started, create a new order with git-pizza init')
+#
+#
+# if len(sys.argv) == 2:
+#     parse_single_args(sys.argv[1])
